7d340c538bb4924b940c704552726cf61bed20ea
DevilPrajwal <prajwalbelkhode216@gmail.com>
2026-02-14 07:09:41 +0530

    Basic program

--- 12_02_2026_Basic_Javascript.js
+++ 12_02_2026_Basic_Javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World!!!");
+a = 10;
+console.log(a);
+
+for (let a = 0; a <= 100; a++)
+{
+    console.log(a);
+    print();
+}
+
+function print()
+{
+    console.log("Hello World!!!");
+}
