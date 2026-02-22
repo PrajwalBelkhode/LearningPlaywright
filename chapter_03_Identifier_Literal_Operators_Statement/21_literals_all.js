300e8227030cc6a4cbd27deb57313ddb8fc62f60
DevilPrajwal <prajwalbelkhode216@gmail.com>
2026-02-22 17:57:01 +0530

    adding literals examples

---
+++ chapter_03_Identifier_Literal_Operators_Statement/21_literals_all.js
@@ -0,0 +1,19 @@
+let single_quotes = 'Hello This is my JS program!!!!';
+console.log(single_quotes);
+
+let double_quotes = "Hello, This is my JS program !!!";
+console.log(double_quotes);
+
+// Template literal
+let first_name = "Prajwal";
+let full_name = `Hi, ${first_name} Belkhode`;
+console.log(full_name);
+
+// Another example for Template literal
+
+let math = `2 + 2 = ${2+2}`;
+console.log(math);
+
+
+let address = "https://app.vwo.com/#/login";
+console.log(address);
