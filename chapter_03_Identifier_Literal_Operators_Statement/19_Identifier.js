67cb7648d4f024dac7d22490381b64117b89ea16
DevilPrajwal <prajwalbelkhode216@gmail.com>
2026-02-22 17:54:58 +0530

    Initial commit for Literal & Operators

---
+++ chapter_03_Identifier_Literal_Operators_Statement/19_Identifier.js
@@ -0,0 +1,18 @@
+// let class = "Hello World";
+// // class is a reserved keyword in JavaScript, so it cannot be used as an identifier. This will result in a syntax error.
+// let if = true;
+// // if is also a reserved keyword in JavaScript, so it cannot be used as an identifier. This will also result in a syntax error.
+
+// let return = 5;
+// // return is also a reserved keyword in JavaScript, so it cannot be used as an identifier. This will also result in a syntax error.
+
+let myVariable = 10;
+console.log(myVariable);
+// myVariable is not a keyword in JavaScript, so it can be used without any issues.
+
+let undefined = 5;
+console.log(undefined); // ⚠️This works but never do this.
+
+/*
+let infinity = 10; // ⚠️This works but never do this.
+*/
