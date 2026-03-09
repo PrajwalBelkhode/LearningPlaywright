d90f462439145caa79d766c8a4f79b8c5b7e6352
DevilPrajwal <prajwalbelkhode216@gmail.com>
2026-03-09 21:54:13 +0530

    Switch statements

---
+++ chapter_06_Switch_Statements/42_Switch.js
@@ -0,0 +1,28 @@
+let day = 3;
+
+switch(day) {
+    case 0:
+        console.log("Sunday - Rest day");
+        break;
+    case 1:
+        console.log("Monday - Sprint Planning");
+        break;
+    case 2:
+        console.log("Tuesday - Development");
+        break;
+    case 3:
+        console.log("Wednesday - Code review");
+        break;
+    case 4:
+        console.log("Thursday - Testing");
+        break;
+    case 5:
+        console.log("Friday — Deployment & Retro");
+        break;
+    case 6:
+        console.log("Saturday - Rest day");
+        break;
+    default:
+        console.log("Invalid day value");
+        break;
+}
