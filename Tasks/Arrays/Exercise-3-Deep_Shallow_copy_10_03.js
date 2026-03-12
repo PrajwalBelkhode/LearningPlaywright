72a820bd1df4c47f2229045a19abc75fe0b30d53
DevilPrajwal <prajwalbelkhode216@gmail.com>
2026-03-12 06:55:41 +0530

    Exercise 3 Deep shallow copy of Array

---
+++ Tasks/Arrays/Exercise-3-Deep_Shallow_copy_10_03.js
@@ -0,0 +1,14 @@
+/**
+ * Exercise 3 — Deep vs Shallow Copy (Tricky)
+ * What is the output and why? How would you fix it?
+ */
+
+let suite1 = [{ name: "login", status: "pass" }];
+//let suite2 = [...suite1];
+let suite2 = suite1.map(s => ({...suite1}));
+suite2[0].status = "fail";
+console.log(suite1[0].status);
+
+// (...) spread operators creates a shallow ccopy. but the objects inside still point to the same memory reference
+
+// Fix: Map + Spread => let suite2 = suite1.map(s => ({...suite1}));
